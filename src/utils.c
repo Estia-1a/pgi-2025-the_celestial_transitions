d1f5866cd0bd1b07285f0ab3c809447f29693814
jmalekir <jack.maleki-raee@etu.estia.fr>
2025-06-13 11:19:01 +0200

    First pixel et dimension (post pertes total de données)

--- src/utils.c
+++ src/utils.c
@@ -1,9 +1,18 @@
 #include <estia-image.h>
+#include <stdlib.h>
 
 #include "utils.h"
 
-/**
- * @brief Here, you have to define functions of the pixel struct : getPixel and setPixel.
- * 
- */
 
+pixelRGB* getPixel(unsigned char* data, int width, int height, int channels, int x, int y) {
+    if (!data || x >= width || y >= height || x < 0 || y < 0) return NULL;
+
+    int index = (y * width + x) * channels;
+    pixelRGB* px = malloc(sizeof(pixelRGB));
+
+    px->R = data[index];
+    px->G = channels >= 2 ? data[index + 1] : 0;
+    px->B = channels >= 3 ? data[index + 2] : 0;
+
+    return px;
+}
--- src/utils.h
+++ src/utils.h
@@ -7,13 +7,11 @@
  * - void setPixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y, pixelRGB* pixel);
  *
 */
-
 typedef struct _pixelRGB {
-  /*
-   * TO COMPLETE
-   */
-   char R ; 
-} pixelRGB ;
+  int R;
+  int G;
+  int B;
+} pixelRGB;
 
 
 #endif
