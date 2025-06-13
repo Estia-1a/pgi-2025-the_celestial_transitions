5c90cdf872ec58871f9ed69e82486c10b1cb374a
jmalekir <jack.maleki-raee@etu.estia.fr>
2025-06-13 11:57:07 +0200

    Min pixel en cours

--- src/main.c
+++ src/main.c
@@ -40,6 +40,9 @@ int main(int argc, char **argv) {
     first_pixel(configuration.filenames[0]);
   }
   
+  if ( strncmp( configuration.command, "min_pixel", 9 ) == 0 ) {
+    min_pixel(configuration.filenames[0]);
+  }
   
   return 0;
 }
