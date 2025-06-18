78f127f0c03569ebc58f80bf3bc06ce4de8fba44
Manoli-po <manoli.pourteau@etu.estia.fr>
2025-06-18 09:48:09 +0200

    feature-main max-component

--- src/features.h
+++ src/features.h
@@ -8,7 +8,6 @@ void dimension(char* sourcepath);
 void helloWorld();
 void second_line(char* sourcepath);
 void first_pixel(char* sourcepath);
-void max_component(char* sourcepath, char t);
 
 void tenth_pixel(char *source_path);
 
@@ -22,17 +21,19 @@ void max_pixel (char *source_path);
 
 void min_component (char *source_path, char t);
 
+void max_component (char *source_path, char t);
+
 void color_red (char *source_path);
 
 void color_green (char *source_path);
+
+void color_blue (char *source_path);
 
 void color_gray (char *source_path);
 
 void color_gray_luminance (char *source_path);
 
 void color_invert(char *source_path);
-
-void color_blue (char *source_path);
 
 void color_desaturate (char *source_path);
 
